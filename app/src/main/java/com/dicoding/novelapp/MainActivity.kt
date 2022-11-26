f6237f3adda13d579d64b0b5784ce31d6951dfc3
unknown <muhammadfarrelpradipta@gmail.com>
2022-11-26 21:41:34 +0700

    About Page

--- app/src/main/java/com/dicoding/novelapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/novelapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dicoding.novelapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -17,6 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         list.addAll(NovelData.listNovel)
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when(selectedMode){
+            R.id.action_about->{
+
+            }
+        }
     }
 
     private fun showRecyclerList() {
